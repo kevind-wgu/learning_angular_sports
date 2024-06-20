365f189829ead7475f3ae20496f9226e55a3f50f
Kevin Dodge <kevin.dodge@wgu.edu>
2024-06-20 12:44:04 -0600

    feat: Basic structure in place

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TeamsComponent } from './teams/teams.component';
+import { ScoreEntryComponent } from './score-entry/score-entry.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SportsComponent } from './sports/sports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/teams', pathMatch: 'full'},
+  {path: 'teams', component: TeamsComponent},
+  {path: 'scores', component: ScoreEntryComponent},
+  {path: 'schedule', component: ScheduleComponent},
+  {path: 'sports', component: SportsComponent},
+];
