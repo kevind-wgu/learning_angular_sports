84122394e9cd8a81325af932283b7ca20b88e0b7
Kevin Dodge <kevin.dodge@wgu.edu>
2024-06-20 12:39:07 -0600

    adding stuff

---
+++ src/app/score-entry/score-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './score-entry.component.html',
+  styleUrl: './score-entry.component.css'
+})
+export class ScoreEntryComponent {
+
+}
