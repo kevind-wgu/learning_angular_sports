dd622116d182ee458e450974681c0069ee1c7c0b
Kevin Dodge <kevin.dodge@wgu.edu>
2024-06-25 16:16:36 -0600

    feat: Schedule partially working

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Schedule, Season, Sport, Team } from '../models';
-import { State, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.store';
-import { Subscription, of, switchMap, tap } from 'rxjs';
+import { Subscription, of } from 'rxjs';
 import { DatastoreService } from '../datastore.service';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgbDatepickerModule],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
 })
@@ -23,11 +24,17 @@ export class ScheduleComponent implements OnInit, OnDestroy {
   subs: Subscription[] = [];
   form!: FormGroup;
 
+  @ViewChild("teamAFocus") teamAFocus!: ElementRef;
+
   constructor(private store: Store<AppState>, private datastore: DatastoreService) {
   }
 
   ngOnInit(): void {
-    this.form = new FormGroup({});
+    this.form = new FormGroup({
+      'teamA': new FormControl('', [Validators.required]),
+      'teamB': new FormControl('', [Validators.required]),
+      'date': new FormControl(new Date().toDateString(), [Validators.required]),
+    });
     this.subs.push(this.store.select('seasons').subscribe(seasonState => {
       this.seasons = seasonState.seasons;
       if (seasonState.currentSeason) {
@@ -69,5 +76,30 @@ export class ScheduleComponent implements OnInit, OnDestroy {
     return this.teams.find(t => t.id === teamId);
   }
 
+  skipSubmit() {
+
+  }
+
+  private getTeamId(abbr: string) : string | undefined {
+    return this.teams.find(t => t.abbr === abbr)?.id;
+  }
+
+  addSchedule() {
+    console.log("Add Schedule", this.form.value);
+    if (this.form.valid) {
+      const sched = {
+        teamAId: this.getTeamId(this.form.value.teamA),
+        teamBId: this.getTeamId(this.form.value.teamB),
+        date: new Date(this.form.value.date),
+      } as Schedule;
+
+      this.schedules.unshift(sched);
+
+      this.form.reset({'teamA':'', 'teamB':'', 'date': this.form.value.date})
+      setTimeout(()=>{
+        this.teamAFocus.nativeElement.focus();
+      });
+    }
+  }
 
 }
